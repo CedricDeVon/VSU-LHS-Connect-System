9fb377b4c58e73d2100d61d525ab16de8aefdc4b
CedricDeVon <vaughncedriclipardoaraneta@gmail.com>
2024-10-22 20:19:44 +0800

    Handled merge commit continued

--- app.vue
+++ app.vue
@@ -10,3 +10,19 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script>
+export default {
+  
+};
+
+</script>
+
+<style>
+
+body {
+  margin: 0;
+  font-family: 'Century Gothic', Arial, sans-serif;
+  background-color: #f0f4f8;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,6 @@ console.log(database)
 
 <template>
   <div>
-    <h1>Back-end Testing</h1>
+    <h1>Back-end</h1>
   </div>
 </template>

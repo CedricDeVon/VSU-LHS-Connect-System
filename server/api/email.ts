50af4dac504b1ae5df60e21b62fa04866ea1cd35
CedricDeVon <vaughncedriclipardoaraneta@gmail.com>
2024-10-22 21:01:16 +0800

    Disabling Google Auth Continued

--- server/api/email.ts
+++ server/api/email.ts
@@ -1,11 +1,4 @@
 
 export default defineEventHandler(async () => {
-  const mail = useMail()
 
-  mail.send({
-    from: 'John Doe',
-    to: 'vaughncedriclipardoaraneta@gmail.com',
-    subject: 'Incredible',
-    text: 'This is an incredible test message',
-  })
 })

8c05c6f293b3d65fba12afa5329253799dc172f1
Jae J. Moon <126632651+Jedpyong@users.noreply.github.com>
2024-10-22 19:42:22 +0800

    added storage.js in the backend folder

---
+++ backend/storage.js
@@ -0,0 +1,8 @@
+import { storage } from './firebase';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+
+export const uploadFile = async (file, path) => {
+    const storageRef = ref(storage, path);
+    await uploadBytes(storageRef, file);
+    return getDownloadURL(storageRef);
+}

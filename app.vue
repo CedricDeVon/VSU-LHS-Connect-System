4b3dac665edd93541f5ff95f151a48fa54721673
Pauline Dejos <111958029+repromantics01@users.noreply.github.com>
2024-10-25 12:09:34 +0800

    Merged with the backend

--- app.vue
+++ app.vue
@@ -1,12 +1,23 @@
-<script setup lang="ts">
 
-</script>
 
 <template>
   <div>
     <NuxtLayout>
       <NuxtPage/>
-      <UiVueSonner/>
-    </NuxtLayout>
+      <NuxtLink to="/login">Go to Login</NuxtLink>
+      <!-- <UiVueSonner/> -->
+    </NuxtLayout> 
+    
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  methods: {
+    goToLogin() {
+      this.$router.push('/login');
+    }
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { EnvironmentConfigurationReader } from "../library/configurationReaders/environmentConfigurationReader";
+
 import * as firestore from 'firebase/firestore';
 import { getApp } from 'firebase/app';
 import { Chronos } from '~/library/utilities/chronos';
@@ -7,20 +9,23 @@ import { Logger } from '~/library/loggers/logger'
 const database = firestore.getFirestore(getApp());
 console.log(database)
 
-async function test() {
-  const results = await useFetch('/api/demo');
-  console.log('CLIENT:');
-  Logger.logDebug(results.data.value);
-  Logger.logDebug(`${results.data.value?.a}`);
-  Logger.logInfo(`${results.data.value?.b}`);
-  Logger.logWarning(`${results.data.value?.c}`);
-  Logger.logError(`${results.data.value?.d}`);
+// async function test() {
+//   const results = await useFetch('/api/demo');
+//   console.log('CLIENT:');
+//   Logger.logDebug(results.data.value);
+//   Logger.logDebug(`${results.data.value?.a}`);
+//   Logger.logInfo(`${results.data.value?.b}`);
+//   Logger.logWarning(`${results.data.value?.c}`);
+//   Logger.logError(`${results.data.value?.d}`);
+// }
 
-}
+// const router = useRouter();
+// router.push('/login');
+
 </script>
 
 <template>
   <div>
-    <button @click=test>Test Me</button>
+    <!-- <button @click=test>Test Me</button> -->
   </div>
 </template>

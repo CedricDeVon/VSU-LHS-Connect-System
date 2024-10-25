2f9b46611acf210fb10387b49328f14edf189179
Pauline Dejos <111958029+repromantics01@users.noreply.github.com>
2024-10-25 16:46:04 +0800

    Add navigation composable with functions for login and back navigation

---
+++ composables/navigation.ts
@@ -0,0 +1,12 @@
+import { useRouter } from 'vue-router';
+
+const goToLogin = (): void => {
+  const router = useRouter();
+  router.push({ name: 'login' });
+};
+
+const handleBackClick = (): void => {
+  window.history.back();
+};
+
+export { goToLogin, handleBackClick };

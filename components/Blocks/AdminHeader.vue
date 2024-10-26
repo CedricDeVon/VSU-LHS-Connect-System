2173bbce8e6f94c9784981d07a9a5de76339eec3
Pauline Dejos <111958029+repromantics01@users.noreply.github.com>
2024-10-27 02:59:48 +0800

    Created block components and admin dashboard

---
+++ components/Blocks/AdminHeader.vue
@@ -0,0 +1,29 @@
+<template>
+    <header class="flex justify-end items-center p-4">
+        <div class="flex items-center gap-5">
+          <img loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/28ac4a43bf80933a5cd7ae9089be6c5d7b9dae50c8259bbef6892eb66f590acf?placeholderIfAbsent=true&apiKey=a61ecd0d5bec4c4f94bc2ce5eda3f7bc"
+            alt="User profile" class="w-8 object-contain aspect-square hover:scale-110 cursor-pointer" />
+            <button @click="goToLogin"
+          class="logout-button px-7 py-3 rounded-lg bg-728B78 text-white hover:bg-neutral-600"
+          aria-label="Log out">
+          Log out
+        </button>
+        </div>
+      </header>
+</template>
+
+<style scoped>
+
+.bg-728B78 {
+  background-color: #728B78;
+}
+
+.bg-728B78:hover {
+  background-color: #556859;
+}
+.logout-button {
+  font-size: 0.875rem;
+  font-family: 'Century Gothic', sans-serif;
+}
+</style>
---
+++ pages/AdminDashboard.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="flex h-screen">
+    <!-- Sidebar -->
+    <AdminSidebar />
+
+    <div class="flex-grow dashboard-page">
+      <!-- Header -->
+      <AdminHeader />
+
+        <!-- Welcome Section -->
+      <section class="welcome-message mt-11">
+          <h1 class="text-4xl font-bold text-green-900" >Welcome to your dashboard</h1>
+          <hr class="mt-3.5 border-2 border-green-900" />
+        </section>
+
+      <!-- Main Dashboard Content -->
+      <main class="main-container">
+        <section class="mt-10 flex gap-5">
+          <article class="flex-col justify-between mt-1">
+            <section class="flex gap-10 justify-between">
+              <div class="flex flex-row w-[80%] justify-between">
+                <div class="flex flex-col items-start gap-5 text-4xl font-bold text-green-900">
+                  <div class="flex flex-row items-center gap-10">
+                    <img loading="lazy"
+                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/7c60d16a8e36909a44f9f80aa566bf7473375c2826a2dd18236b1b237a36b114?placeholderIfAbsent=true&apiKey=a61ecd0d5bec4c4f94bc2ce5eda3f7bc"
+                      alt="Incidents icon" class="w-[30px] object-contain" />
+                    <h2> {{ reportsCount }} Reports</h2>
+                  </div>
+                  <p class="short-description">View reports by class advisers and track report progress.</p>
+                </div>
+              </div>
+              <div class="w-[20%] mt-5 text-white">
+              <button class="w-full py-3 rounded-lg bg-728B78 ease" aria-label="View sections">
+                View
+              </button>
+            </div>
+            </section>
+            <section class="flex gap-10 justify-between">
+              <div class="flex flex-row  w-[80%] justify-between">
+                <div class="flex flex-col items-start gap-5 text-4xl font-bold text-green-900">
+                  <div class="flex flex-row items-center gap-10">
+                    <img loading="lazy"
+                      src="@/assets/icons/reading-book-dark.png"
+                      alt="Incidents icon" class="w-[40px] object-contain" />
+                    <h2> {{ studentsCount }} Students</h2>
+                  </div>
+                  <p class="short-description">Search for a student and view relevant information.</p>
+                </div>
+              </div>
+              <div class="w-[20%] mt-5 text-white">
+              <button class="w-full py-3 rounded-lg bg-728B78 ease" aria-label="View sections">
+                View
+              </button>
+            </div>
+            </section>
+            <section class="flex gap-10 justify-between">
+              <div class="flex flex-row w-[80%] justify-between">
+                <div class="flex flex-col items-start gap-5 text-4xl font-bold text-green-900">
+                  <div class="flex flex-row items-center gap-10">
+                    <img loading="lazy"
+                      src="@/assets/icons/approved-dark.png"
+                      alt="Incidents icon" class="w-[30px] object-contain" />
+                    <h2>{{ accountsCount }} Account Approvals</h2>
+                  </div>
+                  <p class="short-description">See pending account approvals by class advisers and track account status.</p>
+                </div>
+              </div>
+              <div class="w-[20%] mt-5 text-white">
+              <button class="w-full py-3 rounded-lg bg-728B78 ease" aria-label="View sections">
+                View
+              </button>
+            </div>
+            </section>
+          </article>
+        </section>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+import AdminSidebar from '@/components/Blocks/AdminSidebar.vue';
+import AdminHeader from '~/components/Blocks/AdminHeader.vue';
+
+export default {
+  name: 'AdminDashboard',
+  components: {
+    AdminSidebar,AdminHeader
+  },
+  data() {
+    return {
+      reportsCount: 0,
+      studentsCount: 0,
+      accountsCount: 0
+    }
+  }
+};
+</script>
+
+<style scoped>
+.dashboard-page {
+  background-color: #FFFEF1;
+  font-family: 'Century Gothic', sans-serif;
+}
+
+.main-container {
+  padding: 0 150px;
+  padding-right: 160px;
+}
+
+.logout-button {
+  background-color: #FFFEF1;
+  font-size: 0.875rem;
+}
+
+.short-description {
+  font-size: 1.3rem;
+  font-weight: 500;
+  line-height: normal;
+  padding-left: 5rem;
+  
+}
+
+.welcome-message {
+  font-weight: 500;
+  padding: 0 5rem;
+}
+
+h2 {
+  font-size: 2rem;
+}
+
+section {
+  margin-bottom: 4rem;
+}
+
+.bg-728B78 {
+  background-color: #728B78;
+}
+
+.bg-728B78:hover {
+  background-color: #556859;
+}
+
+</style>

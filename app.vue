885e74d37c956659a73ded0ba445019cf499d37c
CedricDeVon <vaughncedriclipardoaraneta@gmail.com>
2024-10-30 22:10:01 +0800

    Continued merge commit handling

--- app.vue
+++ app.vue
@@ -15,20 +15,6 @@ useHead({
   <div>
     <NuxtLayout>
       <NuxtPage/>
-      <NuxtLink to="/login">Go to Login</NuxtLink>
-      <!-- <UiVueSonner/> -->
     </NuxtLayout> 
-    
   </div>
 </template>
-
-<script>
-export default {
-  name: 'App',
-  methods: {
-    goToLogin() {
-      this.$router.push('/login');
-    }
-  }
-};
-</script>

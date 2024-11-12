1b99cb45cab8c94658fa1c807d6a6ef63bcac967
Ysmael John Jajalla <jajallaysmaeljohn@gmail.com>
2024-11-12 14:42:41 +0800

    pushhh

---
+++ pages/adviser/adviserhomepage.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="adviser-page">
+        <AdviserHeader/>
+    </div>
+</template>
+
+
+<style scoped>
+    .adviser-page{
+        background: #fffef1;
+        height: 850px;
+        position: relative;
+        overflow: hidden; 
+        }
+
+    .backPic{
+        position: absolute;
+        width: 900px;
+        height: 1200px;
+        border-radius: 15px;
+        left: 600px;
+        top: 20px;
+        z-index: 0;
+    }
+
+
+    .AY_Sem {
+    color: #000000;
+    text-align: center;
+    font-size: 25px;
+    font-weight: 500;
+    position: absolute;
+    left: 80px;
+    top: 80px;
+  
+    } 
+
+  .gray {
+    background-color: #728B78;
+    }
+
+    .contain{
+        position:absolute;
+        width: 89%;
+        height: 70%;
+        background: rgba(255, 255, 255, 0.89);
+        border-radius: 15px;
+        left: 80px;
+        top: 170px;
+        z-index: 1;
+        box-shadow: 2px 7px 26.6px 0px rgba(0, 0, 0, 0.25);
+        
+    }
+
+    .title{
+        position: absolute;
+        width: 87%;
+        height: 6.1%;
+        background: #265630;
+        border-radius: 15px;
+        left: 95px;
+        top: 135px;
+        z-index: 2;
+    }
+</style>

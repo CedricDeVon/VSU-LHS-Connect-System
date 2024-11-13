134d4a526f62191b3941858f12b230f9cbd28d9c
Jae J. Moon <126632651+Jedpyong@users.noreply.github.com>
2024-11-14 00:53:44 +0800

    tried storing for adding of individual student dunno if it's right

--- stores/studentAdded.ts
+++ stores/studentAdded.ts
@@ -14,59 +14,6 @@ export const studentAddedStore = defineStore('studentAddedStore', () => {
     const birthDate = ref('');
     const profilePic = ref('');
 
-    // const getUserData = () => {
-    //     return {
-    //         userName: userName.value,
-    //         email: email.value,
-    //         password: password.value,
-    //         confirmPassword: confirmPassword.value
-    //     };
-    // }
-
-    // const getAdviserData = () => {
-    //     return {
-    //         firstName: firstName.value,
-    //         middleName: middleName.value,
-    //         lastName: lastName.value,
-    //         suffix: suffix.value,
-    //         hasSuffix: hasSuffix.value,
-    //         birthdate: birthdate.value,
-    //         facultyId: facultyId.value,
-    //         gradeLevel: parseInt(gradeLevel.value),
-    //         sectionName: sectionName.value
-    //     };
-    // }
-
-    // const getAllData = () => {
-    //     return { ...getUserData(), ...getAdviserData() };
-    // }
-
-    // function resetUserData() {
-    //     userName.value = '';
-    //     email.value = '';
-    //     password.value = '';
-    //     confirmPassword.value = '';
-    //     errorMessage.value = '';
-    // }
-
-    // function resetAdviserData() {
-    //     firstName.value = '';
-    //     middleName.value = '';
-    //     lastName.value = '';
-    //     suffix.value = '';
-    //     hasSuffix.value = false;
-    //     birthdate.value = '';
-    //     facultyId.value = '';
-    //     gradeLevel.value = '';
-    //     sectionName.value = '';
-    //     errorMessage.value = '';
-    // }
-  
-    // function resetAllData() {
-    //     resetUserData();
-    //     resetAdviserData();
-    // }
-  
     return { 
         studentId,
         firstName,

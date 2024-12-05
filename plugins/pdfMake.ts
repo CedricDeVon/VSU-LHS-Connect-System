60d344e1c4f16af7e5c14c6f6ad7e62c6593e9bd
CedricDeVon <vaughncedriclipardoaraneta@gmail.com>
2024-12-05 21:29:53 +0800

    Continued Deployment Preparation

--- plugins/pdfMake.ts
+++ plugins/pdfMake.ts
@@ -1,17 +1,20 @@
 import pdfMake from 'pdfmake/build/pdfmake';
-import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+// import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 
-// Initialize pdfMake with fonts
-if (typeof window !== 'undefined') {
-  pdfMake.vfs = pdfFonts.pdfMake.vfs;
-  pdfMake.fonts = {
-    Roboto: {
-      normal: 'Roboto-Regular.ttf',
-      bold: 'Roboto-Medium.ttf',
-      italics: 'Roboto-Italic.ttf',
-      bolditalics: 'Roboto-MediumItalic.ttf'
-    }
-  };
-}
+export default defineNuxtPlugin((nuxtApp) => {
+  // Initialize pdfMake with fonts
+  // if (typeof window !== 'undefined') {
+  //   pdfMake.vfs = pdfFonts.pdfMake.vfs;
+  //   pdfMake.fonts = {
+  //     Roboto: {
+  //       normal: 'Roboto-Regular.ttf',
+  //       bold: 'Roboto-Medium.ttf',
+  //       italics: 'Roboto-Italic.ttf',
+  //       bolditalics: 'Roboto-MediumItalic.ttf'
+  //     }
+  //   };
+  // }
+  // Provide pdfMake globally via `nuxtApp.provide`
+  nuxtApp.provide('pdfMake', pdfMake);
+});
 
-export default pdfMake;

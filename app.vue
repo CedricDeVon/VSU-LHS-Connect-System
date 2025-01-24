74aa7072ebfc412cbf29fb9e46ff5a7ed0eefd7d
CedricDeVon <vaughncedriclipardoaraneta@gmail.com>
2025-01-25 00:31:15 +0800

    Continued initial project setup

---
+++ app.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { createClient } from "@supabase/supabase-js";
+
+const { supabaseUrl, supabaseKey } = useRuntimeConfig().public;
+const supabase = createClient(supabaseUrl, supabaseKey);
+console.log(supabase);
+
+useHead({
+    title: "VSU-LHS Connect System",
+    link: [
+      {
+        rel: "icon",
+        type: "image/webp",
+        href: "/icon.webp",
+      },
+    ],
+});
+
+</script>
+
+<template>
+  <div>
+    
+  </div>
+</template>
